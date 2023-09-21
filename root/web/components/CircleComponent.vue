a43abf9e9e528b998a46a1bad7bc4eb36796a9c1
Adam Moore <adamoore@gmail.com>
2023-09-21 13:26:52 -0400

    circles looking better

--- root/web/components/CircleComponent.vue
+++ root/web/components/CircleComponent.vue
@@ -1,21 +1,24 @@
 <template>
   <ClientOnly>
-    <div v-for="n in numberOfCircles" :key="n" class="rounded-full bg-blue-600 h-10 w-10"></div>
+    <div v-for="n in totalCircles" :key="n" class="rounded-full bg-blue-600 h-10 w-10"></div>
   </ClientOnly>
 </template>
 
 <script setup lang="ts">
   const { width: windowWidth, height: windowHeight } = useWindowSize();
 
-  const windowArea = computed(() => windowHeight.value * windowWidth.value);
-
-  const radius = computed(() => 40 / 2);
-  const circleArea = computed(() => radius.value ** 2 * Math.PI);
-  const numberOfCircles = computed(() => Math.ceil(windowArea.value / circleArea.value));
-
-  onMounted(() => {
-    console.log(numberOfCircles.value);
+  const props = defineProps({
+    gap: {
+      required: true,
+      type: Number,
+    },
   });
+
+  const circleSize = 40 + props.gap;
+
+  const horizontalMax = computed(() => Math.floor(windowWidth.value / circleSize));
+  const verticalMax = computed(() => Math.floor(windowHeight.value / circleSize));
+  const totalCircles = computed(() => horizontalMax.value * verticalMax.value);
 </script>
 
 <style scoped></style>
